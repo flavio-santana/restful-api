17b7b006abb58a1ed8ddff765b74a0fc5e4c6adf
Flavio Santana <f.o.santana@gmail.com>
2020-04-20 17:02:32 -0300

    Criado Model -a para pessoa e imovel

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+//use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResource('/pessoas','PessoaController');
+
+Route::group(['prefix'=>'pessoas'],function(){
+
+    Route::apiResource('/{pessoa}/imoveis','ImovelController');
+
+});
+
