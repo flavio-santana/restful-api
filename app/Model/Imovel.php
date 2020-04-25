57dbf3f622cf7d015f74931298b2457090245845
Flavio Santana <f.o.santana@gmail.com>
2020-04-25 15:55:51 -0300

    Criando relacionamento entre pessoa e imovel

--- app/Model/Imovel.php
+++ app/Model/Imovel.php
@@ -4,7 +4,13 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Model\Pessoa; 
+
 class Imovel extends Model
 {
     //
+    public function pessoas()
+    {
+        $this->belongsTo(Pessoa::class);
+    }
 }
--- app/Model/Pessoa.php
+++ app/Model/Pessoa.php
@@ -4,7 +4,14 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Model\Imovel; 
+
 class Pessoa extends Model
 {
     //
+
+    public function imoveis()
+    {
+        return $this->hasMany(Imovel::class);
+    }
 }
